e04faf603d77821f55ca5160b58898a771b830ea
ndbinh144 <ndbinh144@gmail.com>
2018-12-13 14:04:23 +0700

    ui login

---
+++ frontend/src/views/Authentication.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="authentication">
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'authentication',
+  components: {
+    Login
+  }
+}
+</script>
